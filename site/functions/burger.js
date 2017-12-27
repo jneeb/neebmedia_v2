510f4728dbccba474fbc6cddde45d5a6aa534336
Justin Neeb <neebjustin@gmail.com>
2017-12-27 12:04:56 -0500

    ready for seo

--- site/functions/burger.js
+++ site/functions/burger.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     $("#mobileBurger").click(function () {
         if($("#mobileBurger").hasClass("isOpen")) {
             $(this).removeClass("isOpen");
-            // $("#mobileSlider").removeClass("isOpen");
+            $("#mobileSlider").removeClass("isOpen");
         } else {
             $(this).addClass("isOpen");
-            // $("#mobileSlider").addClass("isOpen");
+            $("#mobileSlider").addClass("isOpen");
         }
     });
 });
